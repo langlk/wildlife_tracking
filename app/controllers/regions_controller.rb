47512f820bff73b3a76a579d85964675a1888cd9
Kelsey Langlois and Kristen Kulha <kelsey+kristen@>
2017-11-13 10:52:58 -0800

    Add regions list and new region view.

---
+++ app/controllers/regions_controller.rb
@@ -0,0 +1,25 @@
+class RegionsController < ApplicationController
+  def index
+    @regions = Region.all
+    render :index
+  end
+
+  def new
+    @region = Region.new
+    render :new
+  end
+
+  def create
+    @region = Region.new(region_params)
+    if @region.save
+      redirect_to regions_path
+    else
+      render :new
+    end
+  end
+
+private
+  def region_params
+    params.require(:region).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :animals do
     resources :sightings
   end
+
+  resources :regions do
+    resources :sightings
+  end
 end
